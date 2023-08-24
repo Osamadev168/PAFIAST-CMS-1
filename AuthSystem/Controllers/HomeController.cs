c5ebc3c00721a289d5778a601a338904c46ccd22
Osama Habib <84494988+Osamadev168@users.noreply.github.com>
2023-08-24 10:21:28 +0500

    fixing ui

--- AuthSystem/Controllers/HomeController.cs
+++ AuthSystem/Controllers/HomeController.cs
@@ -1,11 +1,22 @@
-﻿using AuthSystem.Models;
+﻿using AuthSystem.Data;
+using AuthSystem.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using System.Text.Json.Serialization;
+using System.Text.Json;
 
 namespace AuthSystem.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly AuthDbContext _test;
+
+        public HomeController(AuthDbContext test) {
+
+            _test = test;
+        
+        }
         public IActionResult Index()
         {
             return View();
@@ -29,6 +40,38 @@ namespace AuthSystem.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult GetTestsData() {
+
+
+            try
+            {
+                var testsData = new
+                {
+                    TestList = _test.Tests.OrderByDescending(q => q.Id).ToArray(),
+                    Subjects = _test.Subjects.Include(td => td.Subjects).ToArray(),
+                    TestDetails = _test.TestsDetail.Include(td => td.Test).ToArray(),
+                    TestCalenders = _test.TestCalenders.Where(t => t.Date.Day >= DateTime.UtcNow.Day)
+                                                      .Include(td => td.Test)
+                                                      .Include(td => td.TestCenter)
+                                                      .ToArray()
+                };
+
+                JsonSerializerOptions options = new JsonSerializerOptions
+                {
+                    ReferenceHandler = ReferenceHandler.Preserve
+                    // Other serialization settings if needed
+                };
+
+                return Json(testsData, options);
+            }
+            catch (Exception e)
+            {
+                return Json(new { Error = e.Message });
+            }
+
+
         }
     }
 }
